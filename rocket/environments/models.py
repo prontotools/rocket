084ad10d6fa810a99f15adfa59601a4896d6513b
zkan <kan@prontomarketing.com>
2018-09-04 16:04:40 +0700

    Add project field to environment :bear:

--- rocket/environments/models.py
+++ rocket/environments/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+from projects.models import Project
+
 
 class Environment(models.Model):
     name = models.CharField(max_length=500)
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE
+    )
--- rocket/environments/tests/test_models.py
+++ rocket/environments/tests/test_models.py
@@ -1,14 +1,32 @@
 from django.test import TestCase
 
 from ..models import Environment
+from projects.models import (
+    Project,
+    ProjectGroup,
+)
 
 
 class EnvironmentTest(TestCase):
     def test_save_environment(self):
+        project_group = ProjectGroup.objects.create(
+            name='Pronto World',
+            slug='pronto-world',
+            description='Pronto World Group'
+        )
+        project = Project.objects.create(
+            name='Pronto World',
+            slug='pronto-world',
+            description='Pronto World Description',
+            project_group=project_group
+        )
+
         env = Environment()
         env.name = 'Production'
+        env.project = project
         env.save()
 
         env = Environment.objects.last()
 
         self.assertEqual(env.name, 'Production')
+        self.assertEqual(env.project.id, project.id)
