a26d6ed5e987b34cc59d29b43cf23380eb71df1a
zkan <kan@prontomarketing.com>
2018-09-04 11:12:47 +0700

    Introduce project group and link it to project :bear:

--- rocket/projects/models.py
+++ rocket/projects/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 
+class ProjectGroup(models.Model):
+    name = models.SlugField(max_length=500)
+    slug = models.SlugField(max_length=500)
+    description = models.TextField(null=True, blank=True)
+
+
 class Project(models.Model):
     name = models.SlugField(max_length=500)
     slug = models.SlugField(max_length=500)
     description = models.TextField(null=True, blank=True)
+    project_group = models.ForeignKey(
+        ProjectGroup,
+        on_delete=models.CASCADE
+    )
--- rocket/projects/tests/test_models.py
+++ rocket/projects/tests/test_models.py
@@ -1,10 +1,16 @@
 from django.test import TestCase
 
-from ..models import Project
+from ..models import Project, ProjectGroup
 
 
 class ProjectTest(TestCase):
     def test_save_project(self):
+        project_group = ProjectGroup.objects.create(
+            name='Pronto World',
+            slug='pronto-world',
+            description='Pronto World Group'
+        )
+
         project = Project()
         project.name = 'Pronto World'
         project.slug = 'pronto-world'
@@ -12,6 +18,7 @@ class ProjectTest(TestCase):
             'clients can sign up for services, monitor website analytics, ' \
             'and manage their account.'
         project.description = description
+        project.project_group = project_group
         project.save()
 
         project = Project.objects.last()
@@ -19,3 +26,19 @@ class ProjectTest(TestCase):
         self.assertEqual(project.name, 'Pronto World')
         self.assertEqual(project.slug, 'pronto-world')
         self.assertEqual(project.description, description)
+        self.assertEqual(project.project_group.id, project_group.id)
+
+
+class ProjectGroupTest(TestCase):
+    def test_save_project_group(self):
+        project_group = ProjectGroup()
+        project_group.name = 'Pronto World'
+        project_group.slug = 'pronto-world'
+        project_group.description = 'Pronto World Group'
+        project_group.save()
+
+        project = Project.objects.last()
+
+        self.assertEqual(project_group.name, 'Pronto World')
+        self.assertEqual(project_group.slug, 'pronto-world')
+        self.assertEqual(project_group.description, 'Pronto World Group')
